58148da76389baf93a7671c54e18d2586c7a47f1
Alan Mutalibov <alanasia904@gmail.com>
2024-02-23 19:24:15 +0300

    Removed arts from action

--- scripts/release.bash
+++ scripts/release.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 flutter pub get
 
-if ! flutter test; then
-    echo "Tests failed. Unluck :("
-    exit 1
-fi
-
 if [ ! -d "release" ]; then
     mkdir release
 fi
@@ -13,4 +8,4 @@ fi
 flutter build windows
 cp "build\windows\x64\runner\Release\ci_cd_calculator.exe" "release\ci_cd_calculator.exe"
 
-echo "Сборка приложения завершена"
+echo "Shut up ur mouth"
---
+++ scripts/run_all_tests.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+flutter pub get
+
+if ! flutter test; then
+    echo "Tests failed. Unluck :("
+    exit 1
+fi
+
+echo "All tests pust"

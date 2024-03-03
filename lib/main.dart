9895e8001dfce85f6e1b591b1710d00b77de3579
IBKnight <youtube.integral@mail.ru>
2024-03-03 18:07:19 +0300

    refactoring & small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:ci_cd_calculator/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,10 +7,11 @@ void main() {
     test('subs', () => expect(Calculator.substract(5, 5), 0));
 
     test('default divide', () => expect(Calculator.divide(5, 2), 2.5));
-    test('division by zero', () => expect(Calculator.divide(5, 0), 0));
+    test('division by zero', () => expect(Calculator.divide(5, 0), 'Division by zero'));
 
     test('multiply', () => expect(Calculator.multiply(30, 2), 60));
     test('mod', () => expect(Calculator.mod(7, 2), 1));
+    test('mod with division by zero', () => expect(Calculator.mod(7, 0), 'Division by zero'));
 
     test('ceil', () => expect(Calculator.ceil(4.2), 5));
     test('floor', () => expect(Calculator.floor(3.8), 3));
